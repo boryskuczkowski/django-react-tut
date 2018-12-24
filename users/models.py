efa5aef2e392328a6d4933aed21e254bd20e1a99
unknown <boryskuczkowski@gmail.com>
2018-12-24 19:19:12 +0000

    2nd

--- users/models.py
+++ users/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    is_student = models.BooleanField()
-    is_teacher = models.BooleanField()
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
